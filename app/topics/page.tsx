8889960331841ded7cf12f8c0afbe7f7330d4319
Dayu Liu <ldydyx@gmail.com>
2026-04-23 11:41:25 +0800

    refactor code to fix lint errors

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -22,10 +22,14 @@ export default async function Page() {
           {topicKeys.length === 0 && 'No topics found.'}
           {sortedTopics.map((t) => {
             return (
-              <div key={t} className={`mb-2 mr-5 mt-2
-                  ${t == 'our-papers' ? 
-                    'text-secondary-400 hover:text-secondary-500 dark:text-secondary-300 dark:hover:text-secondary-500'
-                    : 'text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'}
+              <div
+                key={t}
+                className={`mb-2 mr-5 mt-2
+                  ${
+                    t == 'our-papers'
+                      ? 'text-secondary-400 hover:text-secondary-500 dark:text-secondary-300 dark:hover:text-secondary-500'
+                      : 'text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
+                  }
                 `}
               >
                 <Topic key={t} topic={t} />
--- components/Topic.tsx
+++ components/Topic.tsx
@@ -10,9 +10,11 @@ const Tag = ({ topic }: Props) => {
       href={`/topics/${slug(topic)}`}
       /* if extract from post =' Our papers', if extract from topic-data.json = 'our-papers' */
       className={`mr-3 text-sm font-medium uppercase
-        ${(topic == 'Our papers' || topic == 'our-papers') ? 
-          'text-secondary-400 hover:text-secondary-500 dark:text-secondary-300 dark:hover:text-secondary-500'
-          : 'text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'}
+        ${
+          topic == 'Our papers' || topic == 'our-papers'
+            ? 'text-secondary-400 hover:text-secondary-500 dark:text-secondary-300 dark:hover:text-secondary-500'
+            : 'text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
+        }
       `}
     >
       {topic}
